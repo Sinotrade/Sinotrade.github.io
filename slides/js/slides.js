f53862ce0728bced74179cf74c0d7aa674660abd
yvictor <yvictor3141@gmail.com>
2019-03-30 07:26:29 +0000

    slides gallery page done.

--- slides/js/slides.js
+++ slides/js/slides.js
@@ -1,3 +1,12 @@
 
 
+document.querySelectorAll(".gallery").forEach((ele, idx) => {
+    ele.addEventListener('click', (event)=>{
+        node = event.currentTarget
+        console.log(node.getAttribute('target'))
+        window.open(node.getAttribute('target'), '_blank')
+        console.log(node.childNodes)
+    })
+});
+
 fetch("https://api.github.com/repos/Sinotrade/Sinotrade.github.io/git/trees/master?recursive=1").then(res=> res.json()).then(json => {console.log(json["tree"])})
